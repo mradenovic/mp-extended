8419bf09e12604a211e5cf54bc9636b3ecbf8a5f
Mischa Radenovic <miroslav.radenovic@gmail.com>
2017-04-16 23:44:03 -0400

    feat(customer): add Google Maps Directions link

--- app/scripts.babel/customer/load.js
+++ app/scripts.babel/customer/load.js
@@ -37,10 +37,71 @@ function initMapsAPI() {
     );
 }
 
+function getStop(i) {
+  var stop = '';
+  if ($(`#stop${i}Address`).is(':visible')) {
+    stop += $(`#stop${i}Address`).val() + ',';
+    stop += $(`#stop${i}City`).val() + ',';
+    stop += $(`#stop${i}State`).val() + ' ';
+    stop += $(`#stop${i}Zip`).val() + '/';
+  }
+  console.log(stop);
+  return stop;
+}
+
+function getDirectionsLink (options) {
+  var mapsLink = 'https://google.com/maps/dir/';
+
+  if (options.mpeParkingLotFirst && options.mpeParkingLotAddress !== '') {
+    mapsLink += options.mpeParkingLotAddress + '/';
+  }
+
+  for (var i = 1; i < 5; i++) {
+    mapsLink += getStop(i);
+  }
+
+  if (options.mpeParkingLotLast && options.mpeParkingLotAddress !== '') {
+    mapsLink += options.mpeParkingLotAddress + '/';
+  }
+
+  console.log(mapsLink);
+
+  return mapsLink;
+}
+
+function loadDirectionsButton(options) {
+  // select last <tr> in DESTINATION action panel
+  $('.singleBottom').last()
+    // apend new <tr> at the bottom of the DESTINATION action panel
+    .before($('<tr>')
+      .addClass('dataA')
+      // apend <td> and set css
+      .append($('<td>')
+        .css( 'padding-top', '5px' )
+        .css( 'padding-bottom', '5px' )
+        .css( 'background-color', 'LemonChiffon' )
+        // apend <a> and set css
+        .append($('<a>')
+          .addClass('special')
+          .text('Show Directions')
+          .attr('target', '_mp-extended-directions')
+          // dynamicaly set href attribute
+          .click(function() {
+            $(this).attr('href', getDirectionsLink(options));
+          })
+        )
+      )
+    )
+}
+
+
 function init() {
   chrome.storage.sync.get(null, function (options) {
     if (options.mpeMapsAutocomplete) {
       loadCustomerScript();
+    }
+    if (options.mpeDirections) {
+      loadDirectionsButton(options);
     }
   });
 }
--- app/scripts.babel/options/options-services.js
+++ app/scripts.babel/options/options-services.js
@@ -13,37 +13,37 @@ angular.module('optionsApp')
           description: 'Add autocomplete to address fields.',
           disabled: false
         },
-        // mpeDirections: {
-        //   type: 'checkbox',
-        //   value: true,
-        //   label: 'Directions',
-        //   description: 'Add a Directions button in the Charges page. Directions can include a parking lot address and extra stops.',
-        //   disabled: false
-        // },
-        // mpeParkingLotFirst: {
-        //   type: 'checkbox',
-        //   value: false,
-        //   label: 'Parking lot first',
-        //   description: 'Include the parking lot address as a starting point in directions.',
-        //   disabled: '!options.directions.value'
-        // },
-        // mpeParkingLotLast: {
-        //   type: 'checkbox',
-        //   value: false,
-        //   label: 'Parking lot last',
-        //   description: 'Include the parking lot address as a ending point in directions.',
-        //   disabled: '!options.directions.value'
-        // },
-        // mpeParkingLotAddress: {
-        //   type: 'text',
-        //   value: 'NY 11106',
-        //   placeholder: 'Enter the parking lot address',
-        //   class: 'form-control',
-        //   label: 'Parking lot address',
-        //   labelClass: 'sr-only',
-        //   description: 'The address of the parking lot.',
-        //   disabled: '!(options.directions.value && (options.parkingLotFirst.value || options.parkingLotLast.value))'
-        // },
+        mpeDirections: {
+          type: 'checkbox',
+          value: true,
+          label: 'Directions',
+          description: 'Add a Directions button in the Charges page. Directions can include a parking lot address and extra stops.',
+          disabled: false
+        },
+        mpeParkingLotFirst: {
+          type: 'checkbox',
+          value: true,
+          label: 'Parking lot first',
+          description: 'Include the parking lot address as a starting point in directions.',
+          disabled: '!options.mpeDirections.value'
+        },
+        mpeParkingLotLast: {
+          type: 'checkbox',
+          value: false,
+          label: 'Parking lot last',
+          description: 'Include the parking lot address as a ending point in directions.',
+          disabled: '!options.mpeDirections.value'
+        },
+        mpeParkingLotAddress: {
+          type: 'text',
+          value: 'NY 11106',
+          placeholder: 'Enter the parking lot address',
+          class: 'form-control',
+          label: 'Parking lot address',
+          labelClass: 'sr-only',
+          description: 'The address of the parking lot.',
+          disabled: '!(options.mpeDirections.value && (options.mpeParkingLotFirst.value || options.mpeParkingLotLast.value))'
+        },
         // mpeEmailSearch: {
         //   type: 'checkbox',
         //   value: false,
